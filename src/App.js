3af3d9b1627c6f5e0fff6f6ff4aa943f20b36e46
JiyoonSon <sjy4489@korea.ac.kr>
2022-08-27 15:31:44 +0900

    feat: 기본 라우팅 및 라우팅용 임시 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import Navigation from './components/Navigation';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Basket from "./pages/Basket";
+import "./App.css";
 
 function App() {
-  return <div className="App">
-    <Navigation />
-    코멘토 쇼핑
-    </div>;
+  return (
+    <BrowserRouter basename="comento-shop">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

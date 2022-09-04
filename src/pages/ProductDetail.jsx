5ea0b38b0a8fe33b05e8190afd909bacfa662567
JiyoonSon <sjy4489@korea.ac.kr>
2022-09-04 18:27:31 +0900

    design: 상품 상세 페이지 styled 적용

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import Navigation from "../components/Navigation";
 
 const ProductDetail = () => {
-  return <div>상품상세 페이지</div>;
   //URL에서 파라미터 변소(productId) 받아오는 로직
   let { productId } = useParams();
+
+  return (
+    <ProductDetailStyled>
+      <Navigation />
+    </ProductDetailStyled>
+  );
 };
+
+const ProductDetailStyled = styled.div``;
 
 export default ProductDetail;

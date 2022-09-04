2d796f0f03a01ddc2ca34fb74f4568da4e231f4b
JiyoonSon <sjy4489@korea.ac.kr>
2022-09-04 18:31:01 +0900

    feat: 상품 상세 페이지 id로 mock 상품 가져오는 로직 추가

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,10 +2,17 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navigation from "../components/Navigation";
+import { getProductDetail } from "../data/mockData";
 
 const ProductDetail = () => {
   //URL에서 파라미터 변소(productId) 받아오는 로직
   let { productId } = useParams();
+  const [product, setProduct] = useState();
+
+  useEffect(() => {
+    const result = getProductDetail(productId);
+    setProduct(result);
+  }, []);
 
   return (
     <ProductDetailStyled>
